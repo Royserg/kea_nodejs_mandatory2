cf116fe9114cd05d3971859c22e4c92fb9d2021f
Jakub <staniojakub@gmail.com>
2020-05-23 18:26:53 +0200

    Add fields generator as a partial

---
+++ utils/formFieldsKinds.js
@@ -0,0 +1,30 @@
+const username = {
+  label: 'Username',
+  name: 'username',
+  type: 'text',
+}
+
+const email = {
+  label: 'Email',
+  name: 'email',
+  type: 'email',
+}
+
+const password = {
+  label: 'Password',
+  name: 'password',
+  type: 'password',
+}
+
+const confirmPassword = {
+  label: 'Confirm Password',
+  name: 'confirmPassword',
+  type: 'password',
+}
+
+module.exports = {
+  username,
+  email,
+  password,
+  confirmPassword,
+}
